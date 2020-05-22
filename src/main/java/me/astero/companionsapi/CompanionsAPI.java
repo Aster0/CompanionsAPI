cde080e3a2d75f54f1a8d44083fb16c1d61caebd
BuildTools <unconfigured@null.spigotmc.org>
2020-05-23 00:33:45 +0800

    First commit

---
+++ src/main/java/me/astero/companionsapi/CompanionsAPI.java
@@ -0,0 +1,17 @@
+package me.astero.companionsapi;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CompanionsAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/me/astero/companionsapi/api/Test.java
@@ -0,0 +1,10 @@
+package me.astero.companionsapi.api;
+
+public class Test {
+
+
+    public void test()
+    {
+        System.out.println("test");
+    }
+}
